d9dd3cf81cb8e3510975cdcdb6e05ba3662ddbbc
Bhuiyan Mohammad Iklsah <bhuiyan.mohammad.iklsah@gmail.com>
2020-09-27 10:30:18 +0600

    Fix functions

---
+++ mere-store-lib/src/pairkey.h
@@ -0,0 +1,24 @@
+#ifndef PAIRKEY_H
+#define PAIRKEY_H
+
+#include "key.h"
+
+namespace Mere
+{
+namespace Store
+{
+
+class PairKey: public Key
+{
+public:
+    PairKey(const QString &key);    
+
+    bool hasPart() const;
+    int numberOfParts() const;
+    int indexOfNth(int nth) const;
+};
+
+} // namespace Store
+} // namespace Mere
+
+#endif // PAIRKEY_H
